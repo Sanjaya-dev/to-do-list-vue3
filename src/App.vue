370fabc36974428b1c147fccc05979d4b3e022f8
Sanjaya <fernanda.forproject@gmail.com>
2025-03-19 11:10:43 +0700

    finish project

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,55 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+
+  const addTodo = ref<string>("");
+  // const editTodo = ref<string>("");
+  const todolist = ref([]);
+
+  const add = () => {
+    if (addTodo.value.trim() !== ""){
+      todolist.value.push(addTodo.value);
+      addTodo.value = '';
+    }
+  };
+
+  const remove = (index: number) => {
+    todolist.value.splice(index, 1);
+  }
+
+  // const edit = (index: number) => {
+  //   todolist.value[index] = editTodo.value;
+    
+  // }
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
   <main>
-    <TheWelcome />
+    <header>
+      <h1 class="font-bold text-center text-2xl">Todo List App</h1>
+    </header>
+    <div class="flex items-center justify-center">
+      <div class="flex flex-col">
+        <label for="todo">Todo</label>
+        <input v-model="addTodo" class="border mt-2.5" type="text" name="todo" placeholder="Add Todo"/>
+        <button @click="add" class="border mt-2.5 bg-green-500">Add</button>
+        <div id="todo" class="mt-5">
+          <li v-for="(todo,index) in todolist" :key="index" class="list-none">
+            <div class="border p-2">
+              {{ todo }}
+              <div class="flex space-x-4">
+                <button @click="remove(index)" class="border mt-2.5 bg-red-500">Delete</button>
+                <button class="border mt-2.5 bg-blue-500">Edit</button>
+              </div>
+            </div>
+            <br>
+          </li>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
